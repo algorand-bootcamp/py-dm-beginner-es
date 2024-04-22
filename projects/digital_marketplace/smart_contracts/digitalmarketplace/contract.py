7fbb7768b87b2344e0da04837580bb2364d893dc
Evert Diaz <evertdiazb@gmail.com>
2024-04-22 16:52:26 -0500

    content

--- projects/digital_marketplace/smart_contracts/digitalmarketplace/contract.py
+++ projects/digital_marketplace/smart_contracts/digitalmarketplace/contract.py
@@ -1,7 +1,71 @@
-from algopy import ARC4Contract, arc4
+from algopy import ARC4Contract, Asset, Global, Txn, UInt64, arc4, gtxn, itxn
 
 
 class Digitalmarketplace(ARC4Contract):
-    @arc4.abimethod()
-    def hello(self, name: arc4.String) -> arc4.String:
-        return "Hello, " + name
+    asset_id: UInt64
+    unitary_price: UInt64
+
+    # Creación de la app
+    @arc4.abimethod(allow_actions=["NoOp"], create="require")
+    def create_application(self, asset_id: Asset, unitary_price: UInt64) -> None:
+        self.asset_id = asset_id.id
+        self.unitary_price = unitary_price
+
+    # Hacer que el contrato haga opt in al asset
+    @arc4.abimethod
+    def opt_in_to_asset(self, mbr_pay: gtxn.PaymentTransaction) -> None:
+        assert Txn.sender == Global.creator_address
+        assert not Global.current_application_address.is_opted_in(Asset(self.asset_id))
+
+        assert mbr_pay.receiver == Global.current_application_address
+        assert mbr_pay.amount == Global.min_balance + Global.asset_opt_in_min_balance
+
+        itxn.AssetTransfer(
+            xfer_asset=self.asset_id,
+            asset_receiver=Global.current_application_address,
+            asset_amount=0,
+        ).submit()
+
+    # Usuario vendedor defina el precio de venta de lo assets
+    @arc4.abimethod
+    def set_price(self, unitary_price: UInt64) -> None:
+        assert Txn.sender == Global.creator_address
+
+        self.unitary_price = unitary_price
+
+    # Usuario comprador puede hacer la compra de n assets
+    @arc4.abimethod
+    def buy(
+        self,
+        buyer_txn: gtxn.PaymentTransaction,
+        quantity: UInt64,
+    ) -> None:
+        assert self.unitary_price != UInt64(0)
+
+        assert buyer_txn.sender == Txn.sender
+        assert buyer_txn.receiver == Global.current_application_address
+        assert buyer_txn.amount == self.unitary_price * quantity
+
+        itxn.AssetTransfer(
+            xfer_asset=self.asset_id, asset_receiver=Txn.sender, asset_amount=quantity
+        ).submit()
+
+    # Usuario vendedor pueda reclamar las ganancias y assets sobrantes
+    @arc4.abimethod(allow_actions=["DeleteApplication"])
+    def delete_application(self) -> None:
+        assert Txn.sender == Global.creator_address
+
+        # Reclamar el saldo de los productos
+        itxn.AssetTransfer(
+            xfer_asset=self.asset_id,
+            asset_receiver=Global.creator_address,
+            asset_amount=0,
+            asset_close_to=Global.creator_address,
+        ).submit()
+
+        # Reclamar las ganancias de algos
+        itxn.Payment(
+            receiver=Global.creator_address,
+            amount=0,
+            close_remainder_to=Global.creator_address,
+        ).submit()
--- projects/digital_marketplace/tests/digitalmarketplace_test.py
+++ projects/digital_marketplace/tests/digitalmarketplace_test.py
@@ -1,48 +1,204 @@
 import algokit_utils
+import algosdk
 import pytest
-from algokit_utils import get_localnet_default_account
-from algokit_utils.config import config
-from algosdk.v2client.algod import AlgodClient
-from algosdk.v2client.indexer import IndexerClient
+from algokit_utils.beta.account_manager import AddressAndSigner
+from algokit_utils.beta.algorand_client import (
+    AlgorandClient,
+    AssetCreateParams,
+    AssetOptInParams,
+    AssetTransferParams,
+    PayParams,
+)
+from algosdk.atomic_transaction_composer import TransactionWithSigner
 
 from smart_contracts.artifacts.digitalmarketplace.client import DigitalmarketplaceClient
 
 
 @pytest.fixture(scope="session")
-def digitalmarketplace_client(
-    algod_client: AlgodClient, indexer_client: IndexerClient
+def algorand() -> AlgorandClient:
+    """Genera un cliente de Algorand para interactuar con la app"""
+    return AlgorandClient.default_local_net()
+
+
+@pytest.fixture(scope="session")
+def dispenser(algorand: AlgorandClient) -> AddressAndSigner:
+    return algorand.account.dispenser()
+
+
+@pytest.fixture(scope="session")
+def creator(algorand: AlgorandClient, dispenser: AddressAndSigner) -> AddressAndSigner:
+    acct = algorand.account.random()
+
+    algorand.send.payment(
+        PayParams(sender=dispenser.address, receiver=acct.address, amount=10_000_000)
+    )
+
+    return acct
+
+
+@pytest.fixture(scope="session")
+def test_asset_id(algorand: AlgorandClient, creator: AddressAndSigner) -> int:
+    sent_txn = algorand.send.asset_create(
+        AssetCreateParams(sender=creator.address, total=10)
+    )
+    return sent_txn["confirmation"]["asset-index"]
+
+
+@pytest.fixture(scope="session")
+def digital_marketplace_client(
+    algorand: AlgorandClient, creator: AddressAndSigner, test_asset_id: int
 ) -> DigitalmarketplaceClient:
-    config.configure(
-        debug=True,
-        # trace_all=True,
-    )
-
     client = DigitalmarketplaceClient(
-        algod_client,
-        creator=get_localnet_default_account(algod_client),
-        indexer_client=indexer_client,
+        algod_client=algorand.client.algod,
+        sender=creator.address,
+        signer=creator.signer,
     )
 
-    client.deploy(
-        on_schema_break=algokit_utils.OnSchemaBreak.AppendApp,
-        on_update=algokit_utils.OnUpdate.AppendApp,
-    )
+    client.create_create_application(asset_id=test_asset_id, unitary_price=0)
+
     return client
 
 
-def test_says_hello(digitalmarketplace_client: DigitalmarketplaceClient) -> None:
-    result = digitalmarketplace_client.hello(name="World")
-
-    assert result.return_value == "Hello, World"
-
-
-def test_simulate_says_hello_with_correct_budget_consumed(
-    digitalmarketplace_client: DigitalmarketplaceClient, algod_client: AlgodClient
-) -> None:
-    result = (
-        digitalmarketplace_client.compose().hello(name="World").hello(name="Jane").simulate()
+def test_opt_in_to_asset(
+    algorand: AlgorandClient,
+    digital_marketplace_client: DigitalmarketplaceClient,
+    test_asset_id: int,
+    creator: AddressAndSigner,
+):
+    # Revisar que no haya un optin previo
+    pytest.raises(
+        algosdk.error.AlgodHTTPError,
+        lambda: algorand.account.get_asset_information(
+            digital_marketplace_client.app_address, test_asset_id
+        ),
     )
 
-    assert result.abi_results[0].return_value == "Hello, World"
-    assert result.abi_results[1].return_value == "Hello, Jane"
-    assert result.simulate_response["txn-groups"][0]["app-budget-consumed"] < 100
+    mbr_pay_txn = algorand.transactions.payment(
+        PayParams(
+            sender=creator.address,
+            receiver=digital_marketplace_client.app_address,
+            amount=200_000,
+        )
+    )
+
+    sp = algorand.client.algod.suggested_params()
+    sp.fee = 1000
+    result = digital_marketplace_client.opt_in_to_asset(
+        mbr_pay=TransactionWithSigner(txn=mbr_pay_txn, signer=creator.signer),
+        transaction_parameters=algokit_utils.TransactionParameters(
+            foreign_assets=[test_asset_id], suggested_params=sp
+        ),
+    )
+    assert result.confirmed_round
+
+    assert (
+        algorand.account.get_asset_information(
+            digital_marketplace_client.app_address, test_asset_id
+        )["asset-holding"]["amount"]
+        == 0
+    )
+
+
+def test_deposit(
+    algorand: AlgorandClient,
+    digital_marketplace_client: DigitalmarketplaceClient,
+    test_asset_id: int,
+    creator: AddressAndSigner,
+):
+    result = algorand.send.asset_transfer(
+        AssetTransferParams(
+            sender=creator.address,
+            receiver=digital_marketplace_client.app_address,
+            asset_id=test_asset_id,
+            amount=5,
+        )
+    )
+
+    assert result["confirmation"]
+
+    assert (
+        algorand.account.get_asset_information(
+            digital_marketplace_client.app_address, test_asset_id
+        )["asset-holding"]["amount"]
+        == 5
+    )
+
+
+def test_set_price(digital_marketplace_client: DigitalmarketplaceClient):
+    result = digital_marketplace_client.set_price(unitary_price=300_000)
+    assert result.confirmed_round
+
+
+def test_buy(
+    algorand: AlgorandClient,
+    digital_marketplace_client: DigitalmarketplaceClient,
+    test_asset_id: int,
+    creator: AddressAndSigner,
+    dispenser: AddressAndSigner,
+):
+    buyer = algorand.account.random()
+
+    algorand.send.payment(
+        PayParams(sender=dispenser.address, receiver=buyer.address, amount=10_000_000)
+    )
+
+    algorand.send.asset_opt_in(
+        AssetOptInParams(sender=buyer.address, asset_id=test_asset_id)
+    )
+
+    buyer_txn = algorand.transactions.payment(
+        PayParams(
+            sender=buyer.address,
+            receiver=digital_marketplace_client.app_address,
+            amount=3 * 300_000,
+            extra_fee=1000,
+        )
+    )
+
+    result = digital_marketplace_client.buy(
+        quantity=3,
+        buyer_txn=TransactionWithSigner(txn=buyer_txn, signer=buyer.signer),
+        transaction_parameters=algokit_utils.TransactionParameters(
+            foreign_assets=[test_asset_id],
+            sender=buyer.address,
+            signer=buyer.signer,
+        ),
+    )
+
+    assert result.confirmed_round
+
+    assert (
+        algorand.account.get_asset_information(buyer.address, test_asset_id)[
+            "asset-holding"
+        ]["amount"]
+        == 3
+    )
+
+
+def test_delete_application(
+    algorand: AlgorandClient,
+    digital_marketplace_client: DigitalmarketplaceClient,
+    test_asset_id: int,
+    creator: AddressAndSigner,
+    dispenser: AddressAndSigner,
+):
+    before_call_amount = algorand.account.get_information(creator.address)["amount"]
+
+    result = digital_marketplace_client.delete_delete_application(
+        transaction_parameters=algokit_utils.TransactionParameters(
+            foreign_assets=[test_asset_id],
+        )
+    )
+
+    assert result.confirmed_round
+
+    after_call_amount = algorand.account.get_information(creator.address)["amount"]
+
+    assert after_call_amount == before_call_amount + 3 * 300_000 + 200_000 - 3000
+
+    assert (
+        algorand.account.get_asset_information(creator.address, test_asset_id)[
+            "asset-holding"
+        ]["amount"]
+        == 7
+    )
