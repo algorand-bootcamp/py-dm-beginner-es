8be8109c5e4e744a0657fc87f2258325094a6c89
Evert Diaz <evertdiazb@gmail.com>
2024-04-24 17:08:10 -0500

    2-2

--- projects/frontend/src/components/methodCall.tsx
+++ projects/frontend/src/components/methodCall.tsx
@@ -2,9 +2,13 @@ import { buffer } from "stream/consumers"
 
 interface MethodCallInterface {
   methodFunction: () => Promise<void>
+  text: string
 }
 
-const MethodCall = ({ methodFunction }: MethodCallInterface) => {
-  return
+const MethodCall = ({ methodFunction, text }: MethodCallInterface) => {
+
+  return <button className="btn m-2" onClick={methodFunction}>
+    {text}
+    </button>
 }
 export default MethodCall
