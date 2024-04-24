bc5cffbdf6b24b1e7e8a14b119e24ee84668e472
Evert Diaz <evertdiazb@gmail.com>
2024-04-23 23:09:34 -0500

    methods.py updated

--- projects/digital_marketplace/methods.py
+++ projects/digital_marketplace/methods.py
@@ -3,6 +3,21 @@ from algokit_utils.beta.algorand_client import AlgorandClient, AssetCreateParams
 
 from smart_contracts.artifacts.digitalmarketplace.client import DigitalmarketplaceClient
 
+# algorand = AlgorandClient.default_local_net()
+# creator = algorand.account.random()
+# dispenser = algorand.account.dispenser()
+# algorand.send.payment(
+#     PayParams(sender=dispenser.address, receiver=creator.address, amount=10_000_000)
+# )
+# dm_client = DigitalmarketplaceClient(
+#         algod_client=algorand.client.algod,
+#         sender=creator.address,
+#         signer=creator.signer,
+#     )
+
+# create_result = dm_client.create_create_application(asset_id=0, unitary_price=0)
+# print(create_result.tx_info
+
 
 def create(
     algorand: AlgorandClient,
@@ -26,7 +41,7 @@ def create(
     mbr_txn = algorand.transactions.payment(
         PayParams(
             sender=sender,
-            receiver=dm_client.app_address,
+            receiver=create_result.tx_info["application-address"],
             amount=200_000,
             extra_fee=1_000
         )
@@ -42,7 +57,7 @@ def create(
     algorand.send.asset_transfer(
         AssetTransferParams(
             sender=sender,
-            receiver=dm_client.app_address,
+            receiver=create_result.tx_info["application-address"],
             asset_id=asset_id,
             amount=quantity
         )
@@ -54,36 +69,37 @@ def buy(
     algorand: AlgorandClient,
     dm_client: DigitalmarketplaceClient,
     sender: str,
+    app_address: str,
     quantity: int,
     unitary_price: int,
     set_units_left: callable,
-    asset_id: int,
 ) -> None:
     buyer_txn = algorand.transactions.payment(
         PayParams(
             sender=sender,
-            receiver=dm_client.app_address,
+            receiver=app_address,
             amount=quantity*unitary_price,
             extra_fee=1_000
         )
     )
 
+    state = dm_client.get_global_state()
+
     dm_client.buy(
        quantity=quantity,
        buyer_txn=buyer_txn,
          transaction_parameters=algokit_utils.TransactionParameters(
-              foreign_apps=[asset_id]
+              foreign_assets=[state["asset_id"]]
          )
     )
 
-    set_units_left(algorand.account.get_asset_information(dm_client.app_address, asset_id)["asset-holding"]["amount"])
+    set_units_left(algorand.account.get_asset_information(app_address, state["asset_id"])["asset-holding"]["amount"])
 
 def delete_application(
     dm_client: DigitalmarketplaceClient,
-    asset_id: int,
     set_app_id: callable
 ) -> None:
    dm_client.delete_delete_application(transaction_parameters=algokit_utils.TransactionParameters(
-        foreign_assets=[asset_id],
+        foreign_assets=[dm_client.get_global_state()["asset_id"]],
     ))
    set_app_id(0)
